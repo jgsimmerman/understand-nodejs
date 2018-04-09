beac642b9d48c3bddcfd211b1414f9e46b692d55
Jacob Simmerman <jgsimmerman@gmail.com>
2018-04-08 23:22:22 -0400

    Update app.js

--- 4 Modules, Exports, and Require/Code Samples/D5_PrototypalInheritance/Finished/app.js
+++ 4 Modules, Exports, and Require/Code Samples/D5_PrototypalInheritance/Finished/app.js
@@ -1,13 +1,41 @@
-function Person(firstname, lastname) {
-	
-	this.firstname = firstname;
-	this.lastname = lastname;
-	
+// Function Constructors: A Normal Function that is used to construct objects
+// The 'this' variable points a new empty object, and that object is returned from the function automatically
+
+
+class Person {
+    constructor(firstname, lastname) {
+        
+        this.firstname = firstname;
+        this.lastname = lastname;
+      
+    }
+
+    greet() {
+        console.log(`Hello, ${this.firstname} ${this.lastname}`);
+    }
 }
 
-Person.prototype.greet = function() {
-	console.log('Hello, ' + this.firstname + ' ' + this.lastname);
-};
+const john = new Person('John', 'Doe');
+john.greet();
+
+const jane = new Person('Jane', 'Doe');
+jane.greet();
+
+console.log(john.constructor.prototype);
+console.log(jane.__proto__);
+console.log(john.__proto__ === jane.__proto__);
+console.log(john.__proto__ === jane.constructor.prototype);
+
+
+/*	ES5 Code
+function Person(firstname, lastname) {
+    this.firstname = firstname;
+    this.lastname = lastname // this becomes an empty object that you can attach things to when Person is called with "new" keyword
+}
+
+Person.prototype.greet = function(){
+    console.log('Hello, ' + this.firstname + ' ' + this.lastname);
+}
 
 var john = new Person('John', 'Doe');
 john.greet();
@@ -17,4 +45,6 @@ jane.greet();
 
 console.log(john.__proto__);
 console.log(jane.__proto__);
-console.log(john.__proto__ === jane.__proto__);
+console.log(john.__proto__ === jane.__proto__)
+console.log(john.constructor.prototype === john.__proto__)
+*/
