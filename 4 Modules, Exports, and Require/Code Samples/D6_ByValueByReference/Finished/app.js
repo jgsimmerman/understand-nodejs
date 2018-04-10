73bf502cb47601efe53f1589078bc8c9055112af
Jacob Simmerman <jgsimmerman@gmail.com>
2018-04-10 16:14:15 -0400

    Update app.js

--- 4 Modules, Exports, and Require/Code Samples/D6_ByValueByReference/Finished/app.js
+++ 4 Modules, Exports, and Require/Code Samples/D6_ByValueByReference/Finished/app.js
@@ -3,6 +3,27 @@ function change(b) {
 	b = 2;
 }
 
+const a = 1;
+change(a);
+console.log(a);
+
+// pass by reference
+function changeObj(d) {
+	d.prop1 = () => {};
+	d.prop2 = {};
+}
+
+const c = {};
+c.prop1 = {};
+changeObj(c);
+console.log(c);
+
+/*
+// pass by value
+function change(b) {
+	b = 2;
+}
+
 var a = 1;
 change(a);
 console.log(a);
@@ -17,3 +38,4 @@ var c = {};
 c.prop1 = {};
 changeObj(c);
 console.log(c);
+*/
